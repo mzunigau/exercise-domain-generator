94d21340c468a97257c3c3d70e65dc587641f712
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-02-26 02:56:46 +0000

    segunda opcion

---
+++ app2.js
@@ -0,0 +1,24 @@
+console.log("Ejercicio de domain names")
+let pronoun = ['the','our'];
+let adj = ['great', 'big', 'hocomla', 'aleles'];
+let noun = ['jogger','racoon', 'claudio'];
+let domains = ['com','net','org','us', 'es', 'cl'];
+const createDomains = () => {
+    for (p in pronoun) {
+        for (a in adj) {
+            for (n in noun) {
+                for (d in domains){
+                    let str=`${pronoun[p]}${adj[a]}${noun[n] == "claudio" ? "felipe": "Casa" }`
+                    if(str.includes(domains[d])){
+                        let newStr = str.replace(domains[d], "");
+                        finalStr = `${newStr}.${domains[d]}`
+                        console.log(finalStr)
+                    } else {
+                        console.log(`${str}.${domains[d]}`)
+                    }
+                }
+            }
+        }
+    }
+}
+createDomains();
