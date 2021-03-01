264fa267c1a14b5be4b0787fa022b4f4e30f6e35
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-03-01 22:33:13 +0000

    final

--- app.js
+++ app.js
@@ -1,62 +1,27 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
-let noun = ['jogger', 'UsWWW'];
-//'racoon', 'Us','Plus','USA'];
-let end = ['com', 'us']; //, 'net', 'io', 'org']
+let noun = ['jogger', 'UsWWW','racoon', 'Us','Plus','USA'];
+let end = ['com', 'us','net', 'io', 'org']
 
-let domain = [];
 
-//"abc".slice(-1); // "c";
-
-function generateDomain() {
-    let nounLower;
-    
-    for (let i = 0; i < pronoun.length; i++) {
+function generateDomain() {   
+      for (let i = 0; i < pronoun.length; i++) {
         for (let x = 0; x < adj.length; x++) {
             for (let y = 0; y < noun.length; y++) {
-                
-                //theLastofplUs .us  thelastofpl.us
-                 nounLower = noun[y].toLowerCase();
-                 let helper= false;
-                
-                //recorre las extensiones
-
                 for (let z = 0; z < end.length; z++) {
-                    
-                    if (nounLower.includes( end[z] )) {    
-                        //nounLower.search(end[z]);                                  
-                        let res = nounLower.replace(end[z], '');
-                        domain.push(pronoun[i] + adj[x] + res +"."+ end[z]);
-                        helper= true;
-                        break;                        
-                        
-                    }             
+                    let palabra=`${pronoun[i]}${adj[x]}${noun[y]}`                
+                    if(palabra.includes(end[z])){
+                        let rm = palabra.replace(end[z], "");
+                        domainFinal = `${rm}.${end[z]}`
+                        console.log(domainFinal)
+                    } else {
+                        console.log(`${palabra}.${end[z]}`)
+                    }                                         
                     
                 }
-
-                if(!helper){
-
-                    for (let v = 0; v < end.length; v++) {
-                         domain.push(pronoun[i] + adj[x] + noun[y] +"."+end[v]);
-                        
-                    }
-
-
-                }
-                helper=false;
-
-
-
-
-
             }
-
-
-
-
         }
-    }
-    return domain;
+    }    
 }
 
 
---
+++ bkapp.js
@@ -0,0 +1,66 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'UsWWW'];
+//'racoon', 'Us','Plus','USA'];
+let end = ['com', 'us']; //, 'net', 'io', 'org']
+
+let domain = [];
+
+//"abc".slice(-1); // "c";
+
+function generateDomain() {
+    let nounLower;
+    
+    for (let i = 0; i < pronoun.length; i++) {
+        for (let x = 0; x < adj.length; x++) {
+            for (let y = 0; y < noun.length; y++) {
+                
+                //theLastofplUs .us  thelastofpl.us
+                 nounLower = noun[y].toLowerCase();
+                 let helper= false;
+                
+                //recorre las extensiones
+
+                for (let z = 0; z < end.length; z++) {
+                    
+                    if (nounLower.includes( end[z] )) {    
+                          
+                                                 
+                        let res = nounLower.replace(end[z], '');
+
+                        domain.push(pronoun[i] + adj[x] + res +"."+ end[z]);
+                        helper= true;
+                        break;                        
+                        
+                    }             
+                    
+                }
+
+                if(!helper){
+
+                    for (let v = 0; v < end.length; v++) {
+                         domain.push(pronoun[i] + adj[x] + noun[y] +"."+end[v]);                        
+                    }
+
+
+                }
+                helper=false;
+
+
+
+
+
+            }
+
+
+
+
+        }
+    }
+    return domain;
+}
+
+
+
+
+console.log(generateDomain());
